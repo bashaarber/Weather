558a739d410b0c6e34263600766cf9854a93ee26
Arber Basha <arberbasha@Arbers-MacBook-Pro.local>
2019-09-12 23:44:21 +0200

    Aded some files

---
+++ Weather/Controllers/CityViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CityViewController.swift
+//  Weather
+//
+//  Created by Arber Basha on 12/09/2019.
+//  Copyright © 2019 Arber Basha. All rights reserved.
+//
+
+import UIKit
+
+class CityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Weather/Models/CurrentWeather.swift
@@ -0,0 +1,60 @@
+//
+//  CurrentWeather.swift
+//  Weather
+//
+//  Created by Arber Basha on 12/09/2019.
+//  Copyright © 2019 Arber Basha. All rights reserved.
+//
+
+import Foundation
+//
+//{
+//    "coord": {
+//        "lon": 21.28,
+//        "lat": 42.7
+//    },
+//    "weather": [
+//    {
+//    "id": 802,
+//    "main": "Clouds",
+//    "description": "scattered clouds",
+//    "icon": "03n"
+//    }
+//    ],
+//    "base": "stations",
+//    "main": {
+//        "temp": 20.01,
+//        "pressure": 1027,
+//        "humidity": 45,
+//        "temp_min": 19.44,
+//        "temp_max": 20.56
+//    },
+//    "visibility": 10000,
+//    "wind": {
+//        "speed": 2.1,
+//        "deg": 310
+//    },
+//    "clouds": {
+//        "all": 40
+//    },
+//    "dt": 1568311899,
+//    "sys": {
+//        "type": 1,
+//        "id": 95,
+//        "message": 0.1548,
+//        "country": "XK",
+//        "sunrise": 1568261460,
+//        "sunset": 1568307119
+//    },
+//    "timezone": 7200,
+//    "id": 786713,
+//    "name": "Komuna e Prishtinës",
+//    "cod": 200
+//}
+
+
+struct CurrentWeather{
+    
+    
+    
+}
